9a26911fcd0608284f8244e58aae060f7a792014
chuckLcq <15524415221@163.com>
2024-07-01 04:17:03 +0800

    移除网络部分配置

--- mysql/deploy.sh
+++ mysql/deploy.sh
@@ -3,7 +3,6 @@ host=%
 password=123456
 composeVersion=1.0.1.RELEASE
 proxy=https://mirror.ghproxy.com/
-network=structure-network
 imageRepository=mysql
 imagesVersion=8.0.25
 
@@ -12,8 +11,6 @@ wget ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose
 wget ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${composeVersion}/mysql/docker-compose.yml -O $PWD/docker-compose.yml;
 sed -i s/"REPLACE_HOST"/"${host}"/g $PWD/docker-compose.yml && sed -i s/"REPLACE_PASSWORD"/"${password}"/g $PWD/docker-compose.yml;
 sed -i s/"IMAGE_REPOSITORY"/"${imageRepository}"/g $PWD/docker-compose.yml && sed -i s/"IMAGE_VERSION"/"${imagesVersion}"/g $PWD/docker-compose.yml;
-sed -i s/"REPLACE_NETWORK"/"${network}"/g $PWD/docker-compose.yml;
-docker network create ${network};
 docker-compose -f $PWD/docker-compose.yml up -d;
 
 
