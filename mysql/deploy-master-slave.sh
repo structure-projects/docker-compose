6c768c9537697625a285835a21c7050ebe0aea50
chuckLcq <15524415221@163.com>
2024-07-01 15:48:36 +0800

    配置mysql主从的配置

--- mysql/deploy-master-slave.sh
+++ mysql/deploy-master-slave.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-host=$1
-password=$2
-master_host=$3
-version=1.0.1.RELEASE
+host=%
+password=123456
+composeVersion=1.0.1.RELEASE
 proxy=https://mirror.ghproxy.com/
-echo "打印参数: $*"
-#判断传参
-if [ $# -ne 3 ]
-    then
-        echo "参数错误";
-    else
-        echo "执行脚本"
-        mkdir -p $PWD/config && mkdir -p $PWD/data && mkdir -p $PWD/logs && mkdir -p $PWD/init.d
-        wget ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${version}/mysql/master.sql -O ./init.d/init.sql
-        wget ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${version}/mysql/docker-compose.yml -O $PWD/docker-compose.yml;
-        sed -i s/"REPLACE_HOST"/"${host}"/g $PWD/docker-compose.yml && sed -i s/"REPLACE_PASSWORD"/"${password}"/g $PWD/docker-compose.yml
-        docker-compose -f $PWD/docker-compose.yml up -d
-        exit
-fi
+imageRepository=mysql
+imagesVersion=8.0.25
+masterHost=mysql-master
 
 
+mkdir -p $PWD/mysql-master/config && mkdir -p $PWD/mysql-master/data && mkdir -p $PWD/mysql-master/logs && mkdir -p $PWD/mysql-slave/init.d;
+mkdir -p $PWD/mysql-slave/config && mkdir -p $PWD/mysql-slave/data && mkdir -p $PWD/mysql-slave/logs && mkdir -p $PWD/mysql-slave/init.d;
+curl ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${composeVersion}/mysql/master.sql -o $PWD/mysql-master/init.d/init.sql;
+curl ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${composeVersion}/mysql/slave.sql -o $PWD/mysql-slave/init.d/init.sql;
+curl ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${composeVersion}/mysql/master-slave.yml.yml -o $PWD/docker-compose.yml;
+sed -i s/"REPLACE_HOST"/"${host}"/g $PWD/docker-compose.yml && sed -i s/"REPLACE_PASSWORD"/"${password}"/g $PWD/docker-compose.yml;
+sed -i s/"IMAGE_REPOSITORY"/"${imageRepository}"/g $PWD/docker-compose.yml && sed -i s/"IMAGE_VERSION"/"${imagesVersion}"/g $PWD/docker-compose;
+sed -i s/"REPLACE_MASTER_HOST"/"${masterHost}"/g $PWD/docker-compose.yml && sed -i s/"REPLACE_PASSWORD"/"${password}"/g $PWD/mysql-slave/init.d/init.sql;
+docker-compose -f $PWD/docker-compose.yml up -d;
--- mysql/deploy.sh
+++ mysql/deploy.sh
@@ -7,8 +7,8 @@ imageRepository=mysql
 imagesVersion=8.0.25
 
 mkdir -p $PWD/config && mkdir -p $PWD/data && mkdir -p $PWD/logs && mkdir -p $PWD/init.d;
-wget ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${composeVersion}/mysql/master.sql -O ./init.d/init.sql;
-wget ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${composeVersion}/mysql/docker-compose.yml -O $PWD/docker-compose.yml;
+curl ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${composeVersion}/mysql/master.sql -o $PWD/init.d/init.sql;
+curl ${proxy}https://raw.githubusercontent.com/structure-projects/docker-compose/${composeVersion}/mysql/docker-compose.yml -o $PWD/docker-compose.yml;
 sed -i s/"REPLACE_HOST"/"${host}"/g $PWD/docker-compose.yml && sed -i s/"REPLACE_PASSWORD"/"${password}"/g $PWD/docker-compose.yml;
 sed -i s/"IMAGE_REPOSITORY"/"${imageRepository}"/g $PWD/docker-compose.yml && sed -i s/"IMAGE_VERSION"/"${imagesVersion}"/g $PWD/docker-compose.yml;
 docker-compose -f $PWD/docker-compose.yml up -d;
